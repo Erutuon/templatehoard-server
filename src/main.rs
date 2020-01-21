b56026b1d1c98c241580bc06d6db9521b7f99a67
Erutuon <5840197+Erutuon@users.noreply.github.com>
2020-01-21 03:11:02 -0600

    set column width based on grapheme count, add language name

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use std::collections::HashMap;
 use std::fmt::Write;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use warp::{Filter, path, reply::html};
+use warp::{self, Filter, reply::html};
 use regex::Regex;
 use once_cell::sync::Lazy;
 use rlua::{Lua, Result as LuaResult};
+use unicode_segmentation::UnicodeSegmentation;
+
+const ENTRY_LIMIT: usize = 500;
 
 type LanguageCode = String;
 type LanguageName = String;
@@ -13,9 +16,7 @@ static WIKTIONARY_LANGUAGE_CODE_TO_NAME: Lazy<HashMap<LanguageCode, LanguageName
     let lua = Lua::new();
     lua.context(|ctx| {
         let langs: LuaResult<HashMap<LanguageCode, LanguageName>> = ctx.load(r#"
-            require "mediawiki"
-            local langs = require "Module:languages/code_to_name"
-            return langs
+            return require "code_to_name"
         "#).call(());
         langs.unwrap_or_else(|e| {
             panic!("Error while loading table of languages: {}", e);
@@ -23,30 +24,35 @@ static WIKTIONARY_LANGUAGE_CODE_TO_NAME: Lazy<HashMap<LanguageCode, LanguageName
     })
 });
 
+fn get_language_name(language_code: &str) -> &str {
+    WIKTIONARY_LANGUAGE_CODE_TO_NAME.get(language_code)
+        .map(String::as_str)
+        .unwrap_or("?")
+}
+
 markup::define! {
     EntryLinks<'a>(
         lines: Vec<String>,
         lang: String,
         find: Option<&'a str>,
-        find_regex: Option<String>
+        find_regex: Option<String>,
+        max_length: usize
     ) {
         {markup::doctype()}
         html {
             head {
-                title { "Entry name search results"}
-                style {
-                    "a { text-decoration: none; }
-                    ul { column-width: 15em; }"
-                }
+                meta[charset="utf-8"];
+                title { "Entry name search results" }
+                link[rel="stylesheet", type="text/css", href="style.css"];
             }
             body {
                 #main {
-                    p {
-                        {Header { lang, find, find_regex }}
-                    }
-                    ul {
+                    p { { Header { lang, find, find_regex, count: lines.len() } } }
+                    ul[style={format!("column-width: {}ch;", std::cmp::max(max_length, &5))}] {
                         @for line in lines.iter() {
-                            {WiktionaryLink { page: line, lang: &lang }}
+                            li {
+                                {WiktionaryLink { page: line, lang: &lang }}
+                            }
                         }
                     }
                 }
@@ -54,39 +60,46 @@ markup::define! {
         }
     }
     WiktionaryLink<'a>(page: &'a str, lang: &'a str) {
-        li {
-            a[href = {"https://en.wiktionary.org/wiki/".to_string() + page + "#" + lang}] {
-                {page}
-            }
+        a[href = {format!("https://en.wiktionary.org/wiki/{}#{}", page, get_language_name(lang))}] {
+            {page}
         }
     }
     Header<'a>(
         lang: &'a str,
         find: &'a Option<&'a str>,
-        find_regex: &'a Option<String>
-    ) {
-        {{let mut header = format!(
-            "{} entry names",
-            WIKTIONARY_LANGUAGE_CODE_TO_NAME.get(*lang)
-                .map(String::as_str)
-                .unwrap_or("?")
-        );
+        find_regex: &'a Option<String>,
+        count: usize
+    ) {{{
+        let count = *count;
+        let mut header = if count == 0 {
+            "No".to_string()
+        } else if count == ENTRY_LIMIT {
+            format!(
+                "{} {}",
+                if find.is_none() && find_regex.is_none() {
+                    "There are at least "
+                } else {
+                    "At least "
+                },
+                ENTRY_LIMIT
+            )
+        } else {
+            count.to_string()
+        };
+        let language_name = get_language_name(lang);
+        write!(header, " {} entry name", language_name).unwrap();
+        if count != 1 { header.push_str("s"); }
         if let Some(f) = find {
-            write!(header, " containing {}", CodeTag { content: f }).unwrap();
-            if find_regex.is_some() {
-                header.push_str(" and");
-            }
+            write!(header, " contained {}", CodeTag { content: f, class: "bare-str" }).unwrap();
+            if find_regex.is_some() { header.push_str(" and"); }
         }
         if let Some(f) = find_regex {
-            write!(header, " matching the regex {}", CodeTag { content: f }).unwrap();
+            write!(header, " matched the regex {}", CodeTag { content: f, class: "regex" }).unwrap();
         }
-        markup::raw(header)}}
-    }
-    CodeTag<'a>(content: &'a str) {
-        code {
-            {content}
-        }
-    }
+        header.push_str(if count == 0 {"."} else {":"});
+        markup::raw(header)
+    }}}
+    CodeTag<'a>(content: &'a str, class: &'a str) { code.{class} { {content} } }
 }
 
 markup::define! {
@@ -111,48 +124,51 @@ markup::define! {
 }
 
 static LANG_REGEX: Lazy<Regex> = Lazy::new(|| {
-    Regex::new(r"^(?-u)([a-z]{2,3}(?:-[a-z]{2,3}){0,2})$").unwrap()
+    Regex::new(r"^(?-u)[a-z]{2,3}(?:-[a-z]{2,3}){0,2}$").unwrap()
 });
 
 fn list_entries_by_language(filename: String, args: HashMap<String, String>) -> impl warp::reply::Reply {
-    let language_code = match LANG_REGEX.captures(&filename) {
-        Some(c) => c.get(1).unwrap().as_str(),
-        None => return html(format!("Filename is not a language code.")),
+    let language_code = match LANG_REGEX.find(&filename) {
+        Some(c) => c.as_str(),
+        None => return html(format!("{} is not a language code.", &filename)),
     };
     let filename = format!("entries/{}.txt", language_code);
     if let Ok(file) = File::open(&filename) {
         let find_regex: Option<Regex> = match args.get("find_regex") {
-            Some(r) => match Regex::new(&r) {
+            Some(r) if r != "" => match Regex::new(&r) {
                 Ok(r) => Some(r),
                 Err(error) => return html(format!("{}", RegexError { error: error.to_string() })),
             },
-            None => None,
+            _ => None,
         };
-        let find = args.get("find");
+        let find = args.get("find").filter(|f| f.as_str() != "");
         let file = BufReader::new(file);
         let lines: Result<Vec<_>, _> = file.lines()
             .filter(|line| {
                 if let Ok(line) = line {
-                    match find {
-                        Some(find) => line.contains(find),
-                        None => true,
-                    }
+                    find.map(|f| line.contains(f))
+                        .unwrap_or_else(|| {
+                            find_regex.as_ref()
+                                .map(|f| f.is_match(line))
+                                .unwrap_or(true)
+                        })
                 } else {
                     true
                 }
             })
-            .filter(|line| {
-                if let Ok(line) = line {
-                    match &find_regex {
-                        Some(find_regex) => find_regex.is_match(line),
-                        None => true,
-                    }
-                } else {
-                    true
-                }
-            })
-            .take(100)
+            .take(ENTRY_LIMIT)
             .collect();
+        let max_length = lines.as_ref()
+            .ok()
+            .iter()
+            .flat_map(|lines| {
+                lines.iter()
+                    .map(|line| {
+                        UnicodeSegmentation::graphemes(line.as_str(), true).count()
+                    })
+            })
+            .max()
+            .unwrap_or(15usize);
         match lines {
             Ok(lines) => {
                 html(format!(
@@ -160,7 +176,8 @@ fn list_entries_by_language(filename: String, args: HashMap<String, String>) -> 
                     EntryLinks {
                         lines, lang: language_code.to_string(),
                         find: find.map(|s| s.as_str()),
-                        find_regex: find_regex.map(|r| r.as_str().to_string())
+                        find_regex: find_regex.map(|r| r.as_str().to_string()),
+                        max_length,
                     }
                 ))
             },
@@ -173,12 +190,12 @@ fn list_entries_by_language(filename: String, args: HashMap<String, String>) -> 
     }
 }
 
-fn main() {
-    let route = warp::get2()
-        .and(path!("entries" / String))
-        .and(warp::path::end())
+#[tokio::main]
+async fn main() {
+    let route = warp::filters::method::get()
+        .and(warp::path!("entries" / String))
         .and(warp::filters::query::query::<HashMap<String, String>>())
         .map(list_entries_by_language);
 
-    warp::serve(route).run(([127, 0, 0, 1], 3030));
+    warp::serve(route).run(([127, 0, 0, 1], 3030)).await;
 }
