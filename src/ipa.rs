e7478516c7e3bf93e941beb17b5ff70c1e9d406e
Erutuon <5840197+Erutuon@users.noreply.github.com>
2020-01-28 04:11:44 -0600

    fix case of empty "langs" and add regex IPA search

--- src/ipa.rs
+++ src/ipa.rs
@@ -1,16 +1,19 @@
 use crate::html::Page;
+use matches::assert_matches;
 use memmap::Mmap;
+use regex::{Error as RegexError, Regex};
 use serde::{Deserialize, Serialize, Serializer};
 use serde_cbor::Deserializer;
 use serde_urlencoded;
 use std::{
     borrow::Cow,
     collections::{BTreeMap, BTreeSet},
-    convert::{Infallible, TryInto},
+    convert::{Infallible, TryFrom},
     fmt::{Display, Formatter, Result as FmtResult},
     fs::File,
     io::Result as IoResult,
     num::NonZeroUsize,
+    ops::Deref,
     str::FromStr,
 };
 use unicase::UniCase;
@@ -84,23 +87,43 @@ markup::define! {
     }
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 #[serde(deny_unknown_fields)]
 struct Query {
-    search: Option<String>,
     langs: Option<String>,
+    search: Option<String>,
+    regex: Option<String>,
     limit: Option<NonZeroUsize>,
-    offset: Option<usize>,
+    #[serde(default)]
+    offset: usize,
 }
 
-#[derive(Serialize)]
+/*
+enum Matcher {
+    PlainText(String),
+    Regex(Regex),
+}
+
+impl Matcher {
+    fn is_match(&self, s: &str) {
+        match self {
+            Matcher::PlainText(p) => s.contains(p),
+            Matcher::Regex(r) => r.is_match(s),
+        }
+    }
+}
+*/
+
+#[derive(Serialize, Debug)]
 struct Args {
-    search: Option<String>,
     langs: Option<Langs>,
+    search: Option<String>,
+    regex: Option<RegexWrapper>,
     limit: Option<NonZeroUsize>,
     offset: usize,
 }
 
+#[derive(Debug)]
 struct Langs(BTreeSet<String>);
 
 impl Langs {
@@ -111,15 +134,13 @@ impl Langs {
     fn contains(&self, s: &str) -> bool {
         self.0.contains(s)
     }
+
+    fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
 }
 
 struct LangsErr(String);
-
-impl From<LangsErr> for ArgsError {
-    fn from(LangsErr(chars): LangsErr) -> Self {
-        ArgsError::LangsErr(chars)
-    }
-}
 
 impl FromStr for Langs {
     type Err = LangsErr;
@@ -130,7 +151,16 @@ impl FromStr for Langs {
         if bad_chars.peek().is_some() {
             Err(LangsErr(bad_chars.collect()))
         } else {
-            let langs = s.split(',').map(|lang| lang.to_string()).collect();
+            let langs = s
+                .split(',')
+                .filter_map(|lang| {
+                    if lang.is_empty() {
+                        None
+                    } else {
+                        Some(lang.to_string())
+                    }
+                })
+                .collect();
             Ok(Langs(langs))
         }
     }
@@ -155,50 +185,85 @@ impl Serialize for Langs {
     }
 }
 
+#[derive(Debug)]
+struct RegexWrapper(Regex);
+
+impl Deref for RegexWrapper {
+    type Target = Regex;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<Regex> for RegexWrapper {
+    fn from(regex: Regex) -> Self {
+        RegexWrapper(regex)
+    }
+}
+
+impl Serialize for RegexWrapper {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(self.as_str())
+    }
+}
+
 #[test]
 fn test_serialize_args() {
     let args = Args {
-        search: Some("kul".into()),
         langs: "en,de".parse().ok(),
+        search: Some("kul".into()),
+        regex: Some(RegexWrapper(Regex::new("a[aeiou]").unwrap())),
         offset: 0,
-        limit: Some(500),
+        limit: Some(NonZeroUsize::new(500).unwrap()),
     };
     // Check that langs are reordered.
     assert_eq!(
         serde_urlencoded::to_string(args),
-        Ok("search=kul&langs=de%2Cen&offset=0&limit=500".into())
+        Ok(
+            "langs=de%2Cen&search=kul&regex=a%5Baeiou%5D&limit=500&offset=0"
+                .into()
+        )
     );
 }
 
-impl TryInto<Args> for Query {
+impl TryFrom<Query> for Args {
     type Error = ArgsError;
 
-    fn try_into(self) -> Result<Args, Self::Error> {
+    fn try_from(query: Query) -> Result<Self, Self::Error> {
         use ArgsError::*;
 
-        let Self {
-            mut search,
+        let Query {
             langs,
+            mut search,
+            mut regex,
             limit,
             offset,
-        } = self;
+        } = query;
 
-        // Cannot reject an empty search parameter because the form appends
-        // a search parameter even when the input box is empty.
-        if search.as_deref() == Some("") {
-            search = None;
-        }
+        // Cannot reject an empty search or regex parameter because the form
+        // appends a search parameter even when the input box is empty.
+        search = search.filter(|s| !s.is_empty());
+        regex = regex.filter(|s| !s.is_empty());
 
-        if search.is_none() && langs.is_none() {
+        if !(search.is_some() || langs.is_some() || regex.is_some()) {
             Err(MissingParameters)
         } else if is_too_long(&search) || is_too_long(&langs) {
             Err(TooLong)
         } else {
-            let langs = langs.map(|s| s.parse::<Langs>()).transpose()?;
-            let offset = offset.unwrap_or(0);
+            let langs = langs
+                .map(|s| s.parse::<Langs>())
+                .transpose()?
+                .filter(|l| !l.is_empty());
+            let regex =
+                regex.map(|r| Regex::new(&r)).transpose()?.map(|r| r.into());
             Ok(Args {
-                search,
                 langs,
+                search,
+                regex,
                 limit,
                 offset,
             })
@@ -206,11 +271,72 @@ impl TryInto<Args> for Query {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[test]
+fn test_try_into_args() {
+    // Test default values for empty strings in `Query`.
+    let query = Query {
+        langs: Some("".parse().unwrap()),
+        search: Some("faɪnd".into()),
+        regex: Some("".into()),
+        offset: 0,
+        limit: None,
+    };
+    
+    let actual = Args::try_from(query);
+    
+    assert_matches!(
+        actual,
+        Ok(Args {
+            langs: None,
+            search: Some(_),
+            regex: None,
+            offset: 0,
+            limit: None,
+        })
+    );
+    
+    let query = Query {
+        langs: Some("".parse().unwrap()),
+        search: Some("".into()),
+        regex: Some("mæt͡ʃ".into()),
+        offset: 0,
+        limit: None,
+    };
+    
+    let actual = Args::try_from(query);
+    
+    assert_matches!(
+        actual,
+        Ok(Args {
+            langs: None,
+            search: None,
+            regex: Some(_),
+            offset: 0,
+            limit: None,
+        })
+    );
+}
+
+#[derive(Debug, Clone)]
 enum ArgsError {
     MissingParameters,
-    LangsErr(String),
     TooLong,
+    LangsErr(String),
+    RegexError(RegexError),
+}
+
+impl Reject for ArgsError {}
+
+impl From<LangsErr> for ArgsError {
+    fn from(LangsErr(chars): LangsErr) -> Self {
+        ArgsError::LangsErr(chars)
+    }
+}
+
+impl From<RegexError> for ArgsError {
+    fn from(err: RegexError) -> Self {
+        ArgsError::RegexError(err)
+    }
 }
 
 impl Display for ArgsError {
@@ -228,21 +354,20 @@ impl Display for ArgsError {
 ?search=<search text>&langs=<language codes separated by ,>"
                 ),
             ),
-            LangsErr(chars) => write!(
-                f,
-                r#"The "langs" parameter contains the following bad characters: "{}"."#,
-                chars
-            ),
             TooLong => write!(
                 f,
                 r#"Either the "langs" or "search" parameter was too long. The limit is {} bytes."#,
                 MAX_PARAM_LEN
             ),
+            LangsErr(chars) => write!(
+                f,
+                r#"The "langs" parameter contains the following bad characters: "{}"."#,
+                chars
+            ),
+            RegexError(e) => write!(f, "{}", e),
         }
     }
 }
-
-impl Reject for ArgsError {}
 
 const MAX_PARAM_LEN: usize = 256;
 fn is_too_long(param: &Option<String>) -> bool {
@@ -263,8 +388,9 @@ fn mmap_file(path: &str) -> IoResult<Mmap> {
 // is present, the language code (argument 1) must be be found in the list.
 fn ipa_search_results<'a, 'b: 'a>(
     deserializer: impl Iterator<Item = serde_cbor::Result<TemplatesInPage<'a>>>,
-    search: &Option<String>,
     langs: &Option<Langs>,
+    search: &Option<String>,
+    regex: &Option<RegexWrapper>,
     limit: NonZeroUsize,
     offset: usize,
 ) -> BTreeMap<UniCase<&'a str>, Vec<TemplateBorrowed<'a>>> {
@@ -288,6 +414,14 @@ fn ipa_search_results<'a, 'b: 'a>(
                     })
                 })
                 .unwrap_or(true)
+            && regex
+                .as_ref()
+                .map(|r| {
+                    template.parameters.iter().skip(1).any(|(k, v)| {
+                        k.bytes().all(|c| c.is_ascii_digit()) && r.is_match(v)
+                    })
+                })
+                .unwrap_or(true)
     };
     deserializer
         .filter_map(|val| {
@@ -307,8 +441,9 @@ fn ipa_search_results<'a, 'b: 'a>(
 
 fn do_search(
     Args {
-        search,
         langs,
+        search,
+        regex,
         limit,
         offset,
     }: Args,
@@ -323,31 +458,38 @@ fn do_search(
         .unwrap_or(max_limit);
     let results = ipa_search_results(
         Deserializer::from_slice(&text).into_iter(),
-        &search,
         &langs,
+        &search,
+        &regex,
         limit,
         offset,
     );
 
     let mut args = Args {
-        search,
         langs,
+        search,
+        regex,
         limit: Some(limit),
         offset,
     };
-    let mut make_link =
+    let empty_link = |text| format!(r#"<a>{}</a>"#, text);
+    let mut navigation_link =
         |checked_arith: &dyn Fn(usize, usize) -> Option<usize>, text| {
             checked_arith(offset, limit.get())
                 .map(|offset| {
                     args.offset = offset;
                     // Should be infallible because a UTF-8 error is impossible.
                     let query = serde_urlencoded::to_string(&args).unwrap();
-                    Cow::Owned(format!(r#"<a href="?{}">{}</a>"#, query, text))
+                    format!(r#"<a href="?{}">{}</a>"#, query, text)
                 })
-                .unwrap_or(Cow::Borrowed(text))
+                .unwrap_or_else(|| empty_link(text))
         };
-    let prev = make_link(&usize::checked_sub, "prev");
-    let next = make_link(&usize::checked_add, "next");
+    let prev = navigation_link(&usize::checked_sub, "prev");
+    let next = if results.len() >= limit.get() {
+        navigation_link(&usize::checked_add, "next")
+    } else {
+        empty_link("next")
+    };
 
     Page {
         title: "IPA search result",
@@ -396,7 +538,7 @@ async fn print_err(
     use Cow::*;
     use TextOrHTML::*;
     let err_txt = if let Some(e) = err.find::<ArgsError>() {
-        if *e == ArgsError::MissingParameters {
+        if let ArgsError::MissingParameters = *e {
             if let Ok(search_page) = std::fs::read_to_string(&search_page_path)
             {
                 status = Some(StatusCode::OK);
@@ -409,9 +551,11 @@ async fn print_err(
             Text(Owned(e.to_string()))
         }
     } else if err.find::<warp::reject::InvalidQuery>().is_some() {
-        Text(Borrowed(
-            r#"Invalid query string; valid parameters are "search" and "langs".\n"#,
-        ))
+        Text(Borrowed(concat!(
+            "Invalid query string. You may have omitted a parameter ",
+            "or added an invalid one. One of “langs”, “search”, or “regex” ",
+            "is required; “offset” and “limit” are optional.\n"
+        )))
     } else {
         Text(Borrowed("Unknown error\n"))
     };
@@ -428,7 +572,7 @@ pub fn handler<'a>(
 ) -> impl Filter<Extract = (impl Reply,), Error = Infallible> + Clone + 'a {
     warp::query::<Query>()
         .and_then(|params: Query| {
-            async { params.try_into().map_err(warp::reject::custom) }
+            async { Args::try_from(params).map_err(warp::reject::custom) }
         })
         .map(move |args| html(do_search(args, cbor_path, max_limit)))
         .recover(move |e| print_err(e, search_page_path))
